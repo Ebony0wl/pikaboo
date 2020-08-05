e62b45bad357c3a68ca75c30e5b0f6fc0ee8efd8
Combatd <combatdstar@gmail.com>
2020-08-04 23:06:47 -0700

    Uninstall express-session dependency

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
-const session = require('express-session');
+
 const PORT = process.env.PORT || 3000;
 
 
