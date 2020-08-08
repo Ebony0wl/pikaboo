5703cff6c171ef41f3cba296ca901fe636b131be
Combatd <combatdstar@gmail.com>
2020-08-07 19:02:47 -0700

    Define locationsController#index

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,7 +1,21 @@
-const api = 'https://pokeapi.co/api/v2/';
+const api = 'https://pokeapi.co/api/v2/location';
+const axios = require('axios')
 
 const index = (req, res) => {
-    res.json('this will render a list of locations!');
+    axios
+    .get(api + '?limit=50&offset=0')
+    .then((response) => {
+        res.json({
+            locations: response.data.results
+        });
+    })
+    .catch( (err) => {
+        console.log(err);
+        res.json ({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    });
 };
 
 const show = (req, res) => {
