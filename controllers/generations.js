5a62fe9729d87fc60939b46d64c6ea9c25a1d584
Combatd <combatdstar@gmail.com>
2020-08-08 14:08:10 -0700

    Define generationsController#index

--- controllers/generations.js
+++ controllers/generations.js
@@ -1,7 +1,21 @@
-const api = 'https://pokeapi.co/api/v2/';
+const api = 'https://pokeapi.co/api/v2/generation';
+const axios = require('axios')
 
 const index = (req, res) => {
-    res.json('this will render a list of generations!');
+    axios
+    .get(api)
+    .then((response) => {
+        res.json({
+            generations: response.data.results
+        });
+    })
+    .catch( (err) => {
+        console.log(err);
+        res.json ({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    });
 }
 
 const show = (req, res) => {
