38206b37e9e2ef3f8bdd4ddc6be255ed77c93156
Combatd <combatdstar@gmail.com>
2020-08-08 14:38:00 -0700

    Define evolutionsController#index

--- controllers/evolutions.js
+++ controllers/evolutions.js
@@ -1,7 +1,21 @@
-const api = 'https://pokeapi.co/api/v2/';
+const api = 'https://pokeapi.co/api/v2/evolution-chain';
+const axios = require('axios');
 
 const index = (req, res) => {
-    res.json('this will render a list of evolutions!');
+    axios
+    .get(api + '/?limit=419')
+    .then((response) => {
+        res.json({
+            evolutions: response.data.results
+        });
+    })
+    .catch( (err) => {
+        console.log(err);
+        res.json ({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    });
 }
 
 const show = (req, res) => {
