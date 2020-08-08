63df9dc9ceb715e8e0c5b9ee91506203a02e47a5
Combatd <combatdstar@gmail.com>
2020-08-08 16:09:37 -0700

    Define itemsController#show

--- controllers/items.js
+++ controllers/items.js
@@ -19,9 +19,25 @@ const index = (req, res) => {
 }
 
 const show = async (req, res) => {
-    res.json({
-        message: 'You are hitting the items show route! This will display one item.'
-    })
+    const id = req.params.id;
+    console.log(id, ' <-- req.params.id');
+
+    try {
+        const foundItem = await axios.get(`${api}/${id}`);
+        console.log(foundItem.data.names[7], '<-- english information')
+        res.json({
+            type: foundItem.data,
+            name: foundItem.data.name,
+            category_name: foundItem.data.category.name,
+            sprites: foundItem.data.sprites
+        });
+    } catch (err) {
+        console.log(err);
+        res.json ({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    }
 }
 
 module.exports = {
