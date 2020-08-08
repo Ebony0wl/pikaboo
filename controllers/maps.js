4c0d5cb138154e9a1f22f092218e148a6c9856ed
Combatd <combatdstar@gmail.com>
2020-08-07 17:52:14 -0700

    Define mapsController#show

--- controllers/maps.js
+++ controllers/maps.js
@@ -19,8 +19,25 @@ const index = (req, res) => {
     });
 }
 
-const show = (req, res) => {
-    res.json('this will render a specific map!');
+const show = async (req, res) => {
+    const id = req.params.id;
+    console.log(id, ' <-- req.params.id');
+
+    try {
+        const foundMap = await axios.get(`${api}/${id}`);
+        console.log(foundMap.data.names[5], '<-- english information')
+        res.json({
+            type: foundMap.data,
+            name: foundMap.data.names[5].name,
+            locations: foundMap.data.locations
+        });
+    } catch (err) {
+        console.log(err);
+        res.json ({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    }
 }
 
 module.exports = {
